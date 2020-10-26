a16619011334eecdabdd33bd899a8eaeb3b7e29e
Ivv <ivv.jpeg@gmail.com>
2020-10-26 16:13:40 +0200

    ArrayList, LinkedList

---
+++ 2/src/com/company/Lists.java
@@ -0,0 +1,136 @@
+package com.company;
+
+ class Lists {
+    public static class ArrayList<T> {
+        private int init_Size;
+        private int i = 0;
+        private Object[] arr;
+
+        public ArrayList(int num) {
+            init_Size = num;
+            arr = new Object[init_Size];
+        }
+
+        public ArrayList() {
+            init_Size = 10;
+            arr = new Object[init_Size];
+        }
+
+        public int size() {
+            return i;
+        }
+
+        public void resize(int num) {
+            Object[] arr2 = new Object[num];
+
+            if(this.size() < num) {
+                num = this.size();
+            }
+
+            i=0;
+
+            for(int j = 0; j < num; j++) {
+                arr2[j] = arr[j];
+                i++;
+            }
+
+            arr = arr2;
+        }
+
+        public void add(T smt) {
+            if(i == arr.length)
+                resize(i+1);
+
+            arr[i] = smt;
+            i++;
+        }
+
+        public void remove(int index) {
+            if(index > this.size()-1) {
+                System.out.print("Index larger than list size");
+                return;
+            }
+
+            arr[index] = null;
+            for(int j = index; j < this.size(); j++)
+            {
+                T t = (T) arr[j+1];
+                arr[j+1] = arr[j];
+                arr[j] = t;
+            }
+
+        }
+
+        public T get(int index) {
+            return (T) arr[index];
+        }
+
+        public boolean contains(T smt) {
+            for(int j = 0; j < this.size(); j++) {
+                if(arr[j] == smt)
+                    return true;
+            }
+
+            return false;
+        }
+
+    }
+
+    public static class Node<T> {
+        T data;
+        Node next;
+    }
+
+     public static class LinkedList<T> {
+        private Node<T> head;
+        private int size = 0;
+
+        public void add(T d) {
+            Node node = new Node<T>();
+            node.data = d;
+
+            if(head != null) {
+                Node n = head;
+                while(n.next != null) {
+                    n = n.next;
+                }
+                n.next = node;
+                size++;
+            }
+            else {
+                head = node;
+                size++;
+            }
+        }
+
+        public void remove(int index) {
+            if(size-1 < index){
+                System.out.print("Index larger than list size");
+                return;
+            }
+
+            Node n = head;
+            for(int i = 0; i < index - 1; i++){
+                n = n.next;
+            }
+
+            Node n2 = n.next;
+            n.next=n2.next;
+            size--;
+        }
+
+        public T contains(int index){
+            if(size-1 < index){
+                System.out.print("Index larger than list size");
+                return null;
+            }
+
+            Node n = head;
+            for(int i = 0; i < index; i++){
+                n = n.next;
+            }
+
+            return (T) n.data;
+        }
+     }
+}
